80b879f9c55a7ae37887e7294b999d8a9a931e95
FloricaValentin <floricavalentin@yahoo.com>
2024-08-01 05:29:14 +0300

    Update Logo component
    Update Logo component

--- src/components/Home/Logo/Logo.js
+++ src/components/Home/Logo/Logo.js
@@ -1,26 +1,24 @@
 import { useEffect, useRef } from 'react'
-import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
 import LogoS from '../../../assets/images/logo-s.png'
 import './Logo.scss'
 
 const Logo = () => {
   const logoContainerRef = useRef()
-  const outlineLogoRef = useRef()
   const solidLogoRef = useRef()
 
   useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin)
+    const logoContainer = logoContainerRef.current
+    const solidLogo = solidLogoRef.current
 
-    const tl = gsap.timeline()
-    tl.to(logoContainerRef.current, { duration: 1, opacity: 1 })
-      .from(outlineLogoRef.current, { drawSVG: 0, duration: 20 })
+    if (logoContainer) {
+      logoContainer.style.opacity = 1
+    }
 
-    gsap.fromTo(
-      solidLogoRef.current,
-      { opacity: 0 },
-      { opacity: 1, delay: 4, duration: 4 }
-    )
+    if (solidLogo) {
+      setTimeout(() => {
+        solidLogo.style.opacity = 1
+      }, 4000)
+    }
   }, [])
 
   return (
